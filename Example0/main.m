40b43f61ce079e0ef9eaaf479d140b5a81d90156
student <student@APC02HM0Z4DV7N.local>
2014-08-19 11:46:52 -0400

    Initial Commit

---
+++ Example0/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Example0
+//
+//  Created by student on 8/19/14.
+//  Copyright (c) 2014 cs@eku.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
