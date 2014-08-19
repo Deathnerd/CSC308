a836eb970ec2e20f5de60ae9bae2ecb9757dfe37
student <student@APC02HM0Z4DV7N.local>
2014-08-19 11:52:36 -0400

    Initial Commit

--- Example0/main.m
+++ Example0/main.m
@@ -16,6 +16,8 @@ int main(int argc, const char * argv[])
         // insert code here...
         NSLog(@"Hello, World!");
         
+        printf("%s\n", "Have fun with your first Objective-C program!");
+        
     }
     return 0;
 }
